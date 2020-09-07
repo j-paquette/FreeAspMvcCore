9be8d5d95c9862b8a9929f92a8df32ef5b49170c
jmjp775 <jmjp775@gmail.com>
2020-09-07 18:36:32 -0400

    added category, create view

---
+++ FreeCodeCampCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FreeCodeCampCourse.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCodeCampCourse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
