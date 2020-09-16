fc0a4727ead2c7664622fda7b2cc0a402a88e1be
jmjp775 <jmjp775@gmail.com>
2020-09-16 14:06:18 -0400

    still wip, need to add delete features in view

--- FreeCodeCampCourse/Controllers/CategoryController.cs
+++ FreeCodeCampCourse/Controllers/CategoryController.cs
@@ -53,5 +53,82 @@ namespace FreeCodeCampCourse.Controllers
             return View(obj);
            
         }
+
+        //GET - EDIT
+        //The int? means that parameter can be NULL.
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //This checks the values against any validation rules defined in Category.cs model.
+            //If the values are valid, then it will add a new category to the database.
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                //You need to commit first (Entity Framework) to add the new category to the database.
+                _db.SaveChanges();
+                //It's redirecting to the same Controller, so no need to identify it.
+                return RedirectToAction("Index");
+            }
+            //Otherwise, just return the values and its error msg.
+            return View(obj);
+
+        }
+
+        //GET - DELETE
+        //The int? means that parameter can be NULL.
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            //This checks the values against any validation rules defined in Category.cs model.
+            //If the values are valid, then it will add a new category to the database.
+            if (ModelState.IsValid)
+            {
+                var obj = _db.Category.Find(id);
+                if(obj == null)
+                {
+                    return NotFound();
+                }
+                _db.Category.Remove(obj);
+                //You need to commit first (Entity Framework) to add the new category to the database.
+                _db.SaveChanges();
+                //It's redirecting to the same Controller, so no need to identify it.
+                return RedirectToAction("Index");
+            }
+         }
+
     }
 }
